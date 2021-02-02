91af4289915cd89b83d2e00b8c556c85d4650ee2
pramam <pallavi.ramam@gmail.com>
2021-02-01 17:20:14 -0800

    Add grid-cols-{number} text so purge css doesn't strip these classes in a production build

--- components/0-Bubbles/BubbleWrap.tsx
+++ components/0-Bubbles/BubbleWrap.tsx
@@ -8,7 +8,17 @@ export default function BubbleWrap({ rows, cols }: IProps) {
     for (var i = 0; i < rows; i++)
         for (var j = 0; j < cols; j++)
             bubbles.push([i, j])
+
+    // * create this temp variable so purge-css doesn't purge these grid-cols classes for
+    // * a production build.
+    // * https://tailwindcss.com/docs/optimizing-for-production#writing-purgeable-html
+    // * commenting  out the temp variable works, as long as purge-css finds these strings in the code,
+    // * it doesn't purge these classes
+    // const temp = "grid-cols-none grid-cols-1 grid-cols-2 grid-cols-3 grid-cols-4 grid-cols-5 grid-cols-6 grid-cols-7 grid-cols-8 grid-cols-9 grid-cols-10 grid-cols-11 grid-cols-12"
     const grid_props = {
+        // If I pass in grid-cols-${cols} purge css is removing it in a 
+        // production build.
+        // https://tailwindcss.com/docs/optimizing-for-production#writing-purgeable-html
         className: `grid grid-cols-${cols} gap-2`
     }
     return (
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,6 +2,6 @@ import BubbleWrap from '../components/0-Bubbles/BubbleWrap'
 
 export default function projects() {
     return (
-        <BubbleWrap rows={8} cols={12} />
+        <BubbleWrap rows={8} cols={10} />
     )
 }
