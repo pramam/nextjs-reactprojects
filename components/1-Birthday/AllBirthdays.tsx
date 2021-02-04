879248b92314da6f98f6c22fce2a7454dc6fbf51
pramam <pallavi.ramam@gmail.com>
2021-02-03 16:09:07 -0800

    Deleting birthdays using useState

--- components/1-Birthday/AllBirthdays.tsx
+++ components/1-Birthday/AllBirthdays.tsx
@@ -1,26 +1,36 @@
+import { useState } from 'react'
 import { IBirthday } from './definitions'
 import Birthday from './Birthday'
-import birthdaydata from './birthdaydata.json'
+// import birthdaydata from './birthdaydata.json'
 
 
 export interface IProps {
     birthday_array: IBirthday[]
 }
 export default function AllBirthdays({ birthday_array }: IProps) {
+    const [hasBirthdays, setBirthdays] = useState(birthday_array)
+    const clickHandler = () => {
+        setBirthdays([]);
+    }
     return (
         <div>
             <div className="flex justify-center bg-pink-400">
                 <div className="mt-10 mb-10 w-1/3 bg-gray-50 rounded-md shadow-2xl">
                     <div className="flex mb-5 px-5 mt-5">
-                        <h3 className="text-2xl text-gray-900 mb-2">{birthdaydata.length} Birthdays today</h3>
+                        <h3 className="text-2xl text-gray-900 mb-2">{hasBirthdays.length} Birthdays today</h3>
                     </div>
-                    {birthdaydata.map((obj, index) => (
+                    {hasBirthdays.map((obj, index) => (
                         <ul className="px-5 items-center">
                             <Birthday birthday={obj} />
                         </ul>
                     ))}
                     <div className="flex justify-center mb-5">
-                        <button className="justify-center px-32 rounded-md bg-pink-400 text-white py-2 focus:outline-none border border-transparent rounded" type="button">Clear all</button>
+                        <button
+                            className="justify-center px-32 rounded-md bg-pink-400 text-white py-2 focus:outline-none border border-transparent rounded"
+                            type="button"
+                            onClick={clickHandler}>
+                            Clear all
+                        </button>
                     </div>
                 </div>
             </div >
