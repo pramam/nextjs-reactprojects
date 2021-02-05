2a3991c8315e65c075f6a86a44465f3fd5c2014a
pramam <pallavi.ramam@gmail.com>
2021-02-04 18:35:18 -0800

    Increase top margin of bubblewrap

--- components/0-Bubbles/BubbleWrap.tsx
+++ components/0-Bubbles/BubbleWrap.tsx
@@ -22,7 +22,7 @@ export default function BubbleWrap({ rows, cols }: IProps) {
         className: `grid grid-cols-${cols} gap-2`
     }
     return (
-        <div className="container mx-auto max-w-xl px-8 py-8">
+        <div className="container mx-auto max-w-xl px-8 py-8 mt-16">
             <div {...grid_props}>
                 {bubbles.map((rows, index) => (
                     <div>
