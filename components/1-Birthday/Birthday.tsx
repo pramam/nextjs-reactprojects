2411c13ac04d9effc9f8b5dc9e05ca4c356c3391
pramam <pallavi.ramam@gmail.com>
2021-02-04 16:19:15 -0800

    Use object-cover to avoid image distortion

--- components/1-Birthday/Birthday.tsx
+++ components/1-Birthday/Birthday.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from 'next/image'
+// import Image from 'next/image'
 import { IBirthday } from './definitions'
 
 export interface IProps {
@@ -11,7 +11,7 @@ export default function Birthday({ birthday }: IProps) {
         <div>
             <li>
                 <div className="flex mb-5 max-w-xs">
-                    <Image className="rounded-full" src={birthday.image} alt="image" height={70} width={70} />
+                    <img className="rounded-full h-16 w-16 object-cover" src={birthday.image} alt="image" />
                     <div className="self-center pl-4 flex flex-col">
                         <div className="font-semibold">{birthday.name}</div>
                         <div className="text-gray-500 font-semibold text-sm">{birthday.age} years</div>
