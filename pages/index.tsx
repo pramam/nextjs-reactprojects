d7a1f36d2372f45902b314090f6cf35a4c423ceb
pramam <pallavi.ramam@gmail.com>
2021-02-04 19:49:58 -0800

    Making the cards a bit smaller

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,23 +5,22 @@ function allprojects() {
     return (
         <div className="bg-gray-100">
             <div className="flex flex-col justify-center mb-16">
-                <h1 className="mb-2 mt-12 flex justify-center text-4xl font-semibold text-gray-900">React Projects</h1>
+                <h1 className="mb-2 mt-12 flex justify-center text-3xl font-semibold text-gray-900">React Projects</h1>
                 <div className="flex justify-center h-2 rounded-lg mx-auto w-20 bg-blue-800"></div>
             </div>
             <div className="grid grid-cols-3 gap-2">
-                {/* flex-row flex-initial */}
                 {allprojectsData.map((obj, index) => (
                     <a href={obj.url} className="mb-12">
                         <article className="transform hover:scale-105 motion-reduce:transform-none transition duration-500 ease-in-out">
-                            <div className="mt-2 flex flex-col shadow-lg mx-4">
-                                <div className="max-w-28 object-cover">
+                            <div className="mt-2 flex flex-col flex-shrink shadow-lg mx-4">
+                                <div className="max-w-28">
                                     <img
-                                        className="h-80 w-84 object-cover"
+                                        className="h-64 w-70 object-cover"
                                         src={obj.image}
                                         alt={obj.name} />
                                 </div>
                                 <div className="bg-gray-50">
-                                    <div className="mt-8 mb-8 flex justify-center text-3xl font-semibold text-gray-400">{obj.name}</div>
+                                    <div className="m-3 flex justify-center text-xl font-semibold text-gray-400">{obj.name}</div>
                                 </div>
                             </div>
                         </article>
