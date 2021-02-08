971fad08ce23e823f06e5da8a19b870ac7d7d92f
pramam <pallavi.ramam@gmail.com>
2021-02-08 12:35:54 -0800

    Adjusted css; Added tour title and price

--- components/3-Tours/Tour.tsx
+++ components/3-Tours/Tour.tsx
@@ -16,15 +16,20 @@ export default function Tour({ tour }: IProps) {
     // }
     return (
         <div className="flex flex-col justify-center">
-            <div className="max-w-sm sm:max-w-2xl shadow-md mb-5 bg-gray-50">
+            <div className="max-w-sm sm:max-w-2xl shadow-md mb-7 bg-gray-50 rounded-b-md">
                 <img
                     className="w-full h-80 object-cover flex rounded-t-md justify-center items-center"
                     src={tour.image}
                     alt="tour photo"
                 />
                 <span>
-                    <p> {tourinfo.slice(0, 250)}</p>
-                    <button type="button" >See more</button>
+                    <div className="mt-5 mb-5 flex flex-row justify-between">
+                        <h1 className="ml-5 text-md font-bold text-gray-800">{tour.name}</h1>
+                        <h1 className="mr-5 text-md font-bold text-blue-400 bg-blue-100 rounded-full px-2.5 py-0.5">${tour.price}</h1>
+                    </div>
+                    <p className="ml-5 mb-5 mr-5"> {tourinfo.slice(0, 200)}...
+                    <button className="inline-block text-blue-500" type="button" >Read more</button>
+                    </p>
                 </span>
             </div>
 
