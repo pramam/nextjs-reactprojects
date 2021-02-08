35ba6e30e5ee10346cd593a2a10653da8c11103f
pramam <pallavi.ramam@gmail.com>
2021-02-07 16:07:57 -0800

    Put href links to user and repositories

--- components/2-Github/GithubUsers.tsx
+++ components/2-Github/GithubUsers.tsx
@@ -44,6 +44,8 @@ export default function GithubUsers() {
             <h1 className="p-4 flex justify-center text-gray-500 text-lg font-medium uppercase tracking-wide">Github users</h1>
             <ul className="mt-3 p-5 grid grid-cols-2 gap-5 sm:grid-cols-2 lg:grid-cols-4">
                 {users.map((obj, index) => {
+                    const repo_url = obj.html_url + "?tab=repositories";
+
                     return (
                         <li className="bg-gray-50 col-span-1 flex shadow-md rounded-xl"
                             key={index}>
@@ -54,8 +56,12 @@ export default function GithubUsers() {
                                     src={obj.avatar_url}
                                     alt="photo" />
                                 <div className="flex flex-col">
-                                    <h2 className="ml-4 mt-2 text-sm font-medium hover:text-gray-600 text-gray-900">{obj.login}</h2>
-                                    <h4 className="ml-4 mt-2 text-xs font-medium text-gray-500">Repos: {obj.repos_len}</h4>
+                                    <a href={obj.html_url} >
+                                        <h2 className="ml-4 mt-2 text-sm font-medium hover:text-blue-400 text-gray-900">{obj.login}</h2>
+                                    </a>
+                                    <a href={repo_url}>
+                                        <h4 className="ml-4 mt-2 text-xs font-medium text-gray-500 hover:text-blue-400">Repos: {obj.repos_len}</h4>
+                                    </a>
                                 </div>
                             </div>
                         </li>
