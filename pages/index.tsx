934d92bc0a9565443f21ad186c135162fbd26c23
pramam <pallavi.ramam@gmail.com>
2021-02-11 16:40:08 -0800

    Make landing page responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,15 @@ import allprojectsData from '../data/allprojects.json'
 function allprojects() {
     return (
         <div className="bg-gray-100">
-            <div className="flex flex-col justify-center mb-16">
-                <h1 className="mb-2 mt-12 flex justify-center text-3xl font-semibold text-gray-900">React Projects</h1>
-                <div className="flex justify-center h-2 rounded-lg mx-auto w-20 bg-blue-800"></div>
+            <div className="flex flex-col justify-center mb-10 sm:mb-16">
+                <h1 className="mb-2 sm:mb-4 mt-12 flex justify-center text-2xl sm:text-3xl font-semibold text-gray-900">React Projects</h1>
+                <div className="flex justify-center h-1.5 sm:h-2 rounded-lg mx-auto w-10 sm:w-20 bg-blue-800"></div>
             </div>
-            <div className="grid grid-cols-3 gap-2">
+            {/* <div className="flex flex-col justify-center mb-10 sm:mb-16">
+                <h1 className="mb-2 sm:mb-4 mt-12 flex justify-center text-2xl sm:text-5xl font-semibold text-gray-900">Our Tours</h1>
+                <div className="flex justify-center h-1.5 sm:h-2 rounded-lg mx-auto w-10 sm:w-20 bg-blue-800"></div>
+            </div> */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
                 {allprojectsData.map((obj, index) => (
                     <a href={obj.url} className="mb-12">
                         <article className="transform hover:scale-105 motion-reduce:transform-none transition duration-500 ease-in-out">
