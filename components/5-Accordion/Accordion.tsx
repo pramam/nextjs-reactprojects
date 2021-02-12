1af8209fd0ba2a2672a31f6b7b371d1147a27ad4
pramam <pallavi.ramam@gmail.com>
2021-02-12 14:57:34 -0800

    Trying to tweak css a bit

--- components/5-Accordion/Accordion.tsx
+++ components/5-Accordion/Accordion.tsx
@@ -5,17 +5,18 @@ import QuestionCard from './QuestionCard'
 export default function Accordion() {
     return (
         <div className="flex justify-center">
-            <div className="mt-10 sm:w-9/12 bg-gray-100 max-w-5xl shadow-xl rounded-lg">
+            <div className="mt-16 sm:mt-24 sm:w-9/12 bg-gray-100 max-w-5xl shadow-xl rounded-lg">
                 <div className="flex justify-between">
                     <h1 className="pt-2 ml-10 mt-12 w-52 text-3xl font-semibold rounded-xs capitalize">Questions and Answers About My Account </h1>
                     <ul className="mt-12">
                         {accordionData.map((obj, index) => (
                             <li key={index}
-                                className="grid grid-cols-1 mt-4 mb-4 mr-10">
+                                // className="grid grid-cols-1 mt-4 mb-4 mr-10">
+                                //   max-w-xl makes the expanded accordion same size as collapsed one in lg screens
+                                className="max-w-2xl mt-4 mb-4 mr-10">
                                 <QuestionCard card={obj} />
                             </li>
                         ))}
-
                     </ul>
                 </div>
             </div>
