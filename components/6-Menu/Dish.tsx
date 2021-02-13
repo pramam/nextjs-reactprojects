732a0d0ee4e0545273a9a7b213191f43b8ce84bf
pramam <pallavi.ramam@gmail.com>
2021-02-12 19:46:36 -0800

    Get menu with text right for lg screens

--- components/6-Menu/Dish.tsx
+++ components/6-Menu/Dish.tsx
@@ -1,23 +1,24 @@
 export default function Dish({ dish }) {
     return (
-        <div className="lg:max-w-2xl flex justify-center">
-            <div className="flex flex-row">
+        // <div className="lg:max-w-2xl flex justify-center">
+        <div className="lg:grid lg:grid-cols-2 lg:space-x-2 lg:ml-2 lg:mr-2">
+            {/* <div className="flex flex-row"> */}
                 <div className="flex justify-center max-w-xs pr-2 pl-2">
                     <img className="w-full h-40 object-cover flex justify-center items-center"
                         src={dish.image}
                         alt="photo of dish"
                     />
                 </div>
-                {/* <div className="flex flex-col">
+            <div className="flex flex-col">
                     <div className="flex justify-between max-w-md container">
                         <h2 className="uppercase font-semibold">{dish.title}</h2>
                         <h3 className="font-semibold">{dish.price}</h3>
                     </div>
 
-                    <p className="mt-3 font-thin font-sans text-gray-900">{dish.info}</p>
-                </div> */}
-                {/* <hr className="mt-2 h-0.5 rounded-lg bg-gray-500"></hr> */}
+                <p className="mt-3 font-extralight font-sans text-gray-900">{dish.info}</p>
             </div>
+                {/* <hr className="mt-2 h-0.5 rounded-lg bg-gray-500"></hr> */}
+            {/* </div> */}
         </div>
     )
 }
--- components/6-Menu/Menu.tsx
+++ components/6-Menu/Menu.tsx
@@ -5,10 +5,10 @@ export default function Menu() {
     return (
         <div>
             <div className="flex flex-col justify-center mb-10 sm:mb-12">
-                <h1 className="mb-2 sm:mb-4 mt-12 flex justify-center text-2xl sm:text-5xl font-semibold text-gray-900">The Zen Caf&eacute;  </h1>
-                <hr className="flex justify-center h-1.5 sm:h-2 rounded-lg mx-auto w-10 sm:w-20 bg-gray-700"></hr>
+                <h1 className="mb-2 sm:mb-6 mt-12 flex justify-center text-2xl sm:text-5xl font-normal text-gray-900">The Zen Caf&eacute;  </h1>
+                <hr className="flex justify-center h-0.5 rounded-lg mx-auto w-20 sm:w-40 bg-gray-700"></hr>
             </div>
-            <ul className="grid grid-cols-1 lg:grid-cols-2">
+            <ul className="grid grid-cols-1 lg:space-x-2 lg:grid-cols-2">
                 {menuData.map((obj, index) => (
                     <li key={index}
                         className="mb-12"
