ddf50dabdd666e1d4354c3fe6828c987fac93569
pramam <pallavi.ramam@gmail.com>
2021-02-12 21:53:55 -0800

    Fixed image for md:screens; lg is done;

--- components/6-Menu/Dish.tsx
+++ components/6-Menu/Dish.tsx
@@ -1,13 +1,13 @@
 export default function Dish({ dish }) {
     return (
         // <div className="lg:max-w-2xl flex justify-center">
-        <div className="lg:grid lg:grid-cols-2 lg:space-x-2 lg:ml-2 lg:mr-2">
+        <div className="grid grid-cols-1 lg:grid-cols-2 lg:space-x-2 lg:ml-2 lg:mr-2">
             {/* <div className="flex flex-row"> */}
-                <div className="flex justify-center max-w-xs pr-2 pl-2">
-                    <img className="w-full h-40 object-cover flex justify-center items-center"
-                        src={dish.image}
-                        alt="photo of dish"
-                    />
+            <div className="flex justify-center pr-2 pl-2  lg:pr-2 lg:pl-2">
+                <img className="w-full md:max-w-md lg:w-full h-40 object-cover flex justify-center items-center"
+                     src={dish.image}
+                     alt="photo of dish"
+                />
                 </div>
             <div className="flex flex-col">
                     <div className="flex justify-between max-w-md container">
