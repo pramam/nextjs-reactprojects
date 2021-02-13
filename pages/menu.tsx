747b48813ac8767caf8836f7a0e2f09f042b5b4e
pramam <pallavi.ramam@gmail.com>
2021-02-12 18:38:50 -0800

    Menu initial checkin; images are sized equally

---
+++ pages/menu.tsx
@@ -0,0 +1,9 @@
+import Menu from '../components/6-Menu/Menu'
+
+export default function DisplayMenu() {
+    return (
+        <div className="min-h-screen bg-gradient-to-r from-gray-300 via-red-100 to-gray-300">
+            <Menu />
+        </div>
+    )
+}
