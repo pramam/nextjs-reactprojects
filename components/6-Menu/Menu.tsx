032d144754a3f7118d136eb6a19221b55f8811eb
pramam <pallavi.ramam@gmail.com>
2021-02-13 21:16:54 -0800

    Add button click handlers

--- components/6-Menu/Menu.tsx
+++ components/6-Menu/Menu.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react'
 import menuData from './menudata.json'
 import Dish from './Dish'
 
 export default function Menu() {
+    const [menu, setMenu] = useState(menuData);
+    const [display, setDisplay] = useState(menuData);
+
+    const allHandler = () => {
+        setDisplay(menu);
+    }
+
+    const beverageHandler = () => {
+        const beverages = menu.filter(item => item.category == 'beverage');
+        setDisplay(beverages)
+    }
+
+    const breakfastHandler = () => {
+        const breakfast = menu.filter(item => item.category == 'breakfast');
+        setDisplay(breakfast)
+    }
+
+    const lunchHandler = () => {
+        const lunch = menu.filter(item => item.category == 'lunch');
+        setDisplay(lunch)
+    }
+
     return (
         <div>
             <div className="flex flex-col justify-center mb-10 sm:mb-12">
@@ -10,29 +33,33 @@ export default function Menu() {
                 <div className="flex justify-center space-x-2 lg:space-x-4">
                     <button
                         type="button"
-                        className="hover:bg-gray-400 px-1 rounded"
+                        className="hover:bg-gray-400 px-1 rounded focus:outline-none border border-transparent"
+                        onClick={allHandler}
                     ><h2>All</h2>
                     </button>
                     <button
                         type="button"
-                        className="hover:bg-gray-400 px-1 rounded"
+                        className="hover:bg-gray-400 px-1 rounded focus:outline-none border border-transparent"
+                        onClick={beverageHandler}
                     ><h2>Beverages</h2>
                     </button>
                     <button
                         type="button"
-                        className="hover:bg-gray-400 px-1 rounded"
+                        className="hover:bg-gray-400 px-1 rounded focus:outline-none border border-transparent"
+                        onClick={breakfastHandler}
                     ><h2>Breakfast</h2>
                     </button>
                     <button
                         type="button"
-                        className="hover:bg-gray-400 px-1 rounded"
+                        className="hover:bg-gray-400 px-1 rounded focus:outline-none border border-transparent"
+                        onClick={lunchHandler}
                     ><h2>Lunch</h2>
                     </button>
                 </div>
             </div>
             <ul className="flex justify-center xl:justify-between flex-wrap">
 
-                {menuData.map((obj, index) => (
+                {display.map((obj, index) => (
                     <li key={index}
                         className="mb-12 lg:max-w-xl"
                     >
