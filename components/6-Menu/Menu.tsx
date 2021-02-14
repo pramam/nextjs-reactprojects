0154a843e4b78005b8273593ec43fe9373552e31
pramam <pallavi.ramam@gmail.com>
2021-02-14 11:25:53 -0800

    Changed Menu button focus ring styling

--- components/6-Menu/Menu.tsx
+++ components/6-Menu/Menu.tsx
@@ -19,29 +19,29 @@ export default function Menu() {
         <div>
             <div className="flex flex-col justify-center mb-10 sm:mb-12">
                 <h1 className="mb-2 sm:mb-6 mt-12 flex justify-center text-2xl sm:text-5xl font-normal text-gray-900">The Zen Caf&eacute;  </h1>
-                <hr className="mb-2 flex justify-center h-0.5 rounded-lg mx-auto w-20 sm:w-40 bg-gray-700"></hr>
+                <hr className="mb-4 flex justify-center h-0.5 rounded-lg mx-auto w-20 sm:w-40 bg-gray-700"></hr>
                 <div className="flex justify-center space-x-2 lg:space-x-4">
                     <button
                         type="button"
-                        className="hover:bg-gray-400 px-1 rounded focus:outline-none border border-transparent"
+                        className="hover:bg-gray-400 px-1 rounded focus:outline-none focus:ring-1 focus:ring-gray-600 border border-transparent"
                         onClick={allHandler}
                     ><h2>All</h2>
                     </button>
                     <button
                         type="button"
-                        className="hover:bg-gray-400 px-1 rounded focus:outline-none border border-transparent"
+                        className="hover:bg-gray-400 px-1 rounded focus:outline-none focus:ring-1 focus:ring-gray-600 border border-transparent"
                         onClick={() => clickHandler('beverage')}
                     ><h2>Beverages</h2>
                     </button>
                     <button
                         type="button"
-                        className="hover:bg-gray-400 px-1 rounded focus:outline-none border border-transparent"
+                        className="hover:bg-gray-400 px-1 rounded focus:outline-none focus:ring-1 focus:ring-gray-600 border border-transparent"
                         onClick={() => clickHandler('breakfast')}
                     ><h2>Breakfast</h2>
                     </button>
                     <button
                         type="button"
-                        className="hover:bg-gray-400 px-1 rounded focus:outline-none border border-transparent"
+                        className="hover:bg-gray-400 px-1 rounded focus:outline-none focus:ring-1 focus:ring-gray-600 border border-transparent"
                         onClick={() => clickHandler('lunch')}
                     ><h2>Lunch</h2>
                     </button>
