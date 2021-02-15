365e33cb9816255dd498eaf3c6188a045082f437
pramam <pallavi.ramam@gmail.com>
2021-02-14 21:26:53 -0800

    Number the projects in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,6 @@ function allprojects() {
                 <h1 className="mb-2 sm:mb-4 mt-12 flex justify-center text-2xl sm:text-3xl font-semibold text-gray-900">React Projects</h1>
                 <div className="flex justify-center h-1.5 sm:h-2 rounded-lg mx-auto w-10 sm:w-20 bg-blue-800"></div>
             </div>
-            {/* <div className="flex flex-col justify-center mb-10 sm:mb-16">
-                <h1 className="mb-2 sm:mb-4 mt-12 flex justify-center text-2xl sm:text-5xl font-semibold text-gray-900">Our Tours</h1>
-                <div className="flex justify-center h-1.5 sm:h-2 rounded-lg mx-auto w-10 sm:w-20 bg-blue-800"></div>
-            </div> */}
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
                 {allprojectsData.map((obj, index) => (
                     <a href={obj.url} className="mb-12">
@@ -24,7 +20,7 @@ function allprojects() {
                                         alt={obj.name} />
                                 </div>
                                 <div className="bg-gray-50">
-                                    <div className="m-3 flex justify-center text-xl font-semibold text-gray-400">{obj.name}</div>
+                                    <div className="m-3 flex justify-center text-xl font-semibold text-gray-400 capitalize">{index + 1}. {obj.name}</div>
                                 </div>
                             </div>
                         </article>
