76f9721aef308f41c26025b156d9dcc492d095b7
pramam <pallavi.ramam@gmail.com>
2021-02-16 15:15:02 -0800

    Slider: Added icon credits footer

--- pages/slider.tsx
+++ pages/slider.tsx
@@ -2,8 +2,13 @@ import Slider from '../components/9-Slider/Slider'
 
 export default function DisplaySlider() {
     return (
-        <div className="min-h-screen bg-gray-900 ">
-            <Slider />
+        <div className="relative min-h-screen bg-gray-900">
+            <div className="flex flex-col justify-center ">
+                <Slider />
+                <footer className="flex justify-center text-center">
+                    <div className="absolute text-gray-600 bottom-0">Icons made by <a href="https://www.freepik.com" title="Freepik"> &nbsp; Freepik &nbsp; </a> from &nbsp;<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                </footer>
+            </div>
         </div>
     )
 }
