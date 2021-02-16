97b8a06e505c50d856d7d46eeef00ddf35492ed5
pramam <pallavi.ramam@gmail.com>
2021-02-16 15:04:33 -0800

    Slider: Add comments to css code

--- components/9-Slider/OneSlide.tsx
+++ components/9-Slider/OneSlide.tsx
@@ -5,6 +5,7 @@ export default function OneSlide({ data }) {
         <div className="flex flex-col lg:flex-row">
             <div className="flex flex-col-reverse lg:flex-row ">
                 <div className="flex justify-center">
+                    {/* TextCard: */}
                     <div className="lg:max-h-80 lg:transform lg:translate-y-8 shadow-2xl lg:z-30 text-gray-50 bg-gray-800 lg:rounded-md">
                         <div className="flex flex-col">
                             <div className="flex flex-row">
@@ -20,6 +21,7 @@ export default function OneSlide({ data }) {
                     </div>
                 </div>
                 <div className="flex justify-center">
+                    {/* ImageCard */}
                     <div className="max-w-36 px-20 lg:px-0 lg:max-w-xs lg:max-h-96 lg:z-20 lg:transform lg:-translate-x-12">
                         <img
                             className="h-72 lg:rounded-md lg:h-full object-cover flex justify-center items-center"
