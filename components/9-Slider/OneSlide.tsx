f684855de25e790910c8d1c355eebdfdbce9c45e
pramam <pallavi.ramam@gmail.com>
2021-02-15 21:42:14 -0800

    Slider: Adjust tabs so the <div>s align properly

--- components/9-Slider/OneSlide.tsx
+++ components/9-Slider/OneSlide.tsx
@@ -3,35 +3,32 @@ import SVGLeftQuote from "../svgicons/SVGLeftQuote";
 export default function OneSlide({ data }) {
     return (
         <div className="flex flex-col lg:flex-row">
-            {/* <div className="flex justify-center"> */}
-                {/* flex-wrap-reverse is so that the image comes on top in <= md: screens */}
             <div className="flex flex-col-reverse lg:flex-row ">
-            <div className="flex justify-center">
-                <div className="lg:max-h-80 lg:transform lg:translate-y-8 shadow-2xl z-30 text-gray-50 bg-gray-800 lg:rounded-md">
-                    <div className="flex flex-col">
-                        <div className="flex flex-row">
-                            <div className="ml-2 mt-10">
+                <div className="flex justify-center">
+                    <div className="lg:max-h-80 lg:transform lg:translate-y-8 shadow-2xl z-30 text-gray-50 bg-gray-800 lg:rounded-md">
+                        <div className="flex flex-col">
+                            <div className="flex flex-row">
+                                <div className="ml-2 mt-10">
                                     <SVGLeftQuote css="h-7 w-7 text-white" />
-                            </div>
-                            <div>
-                                <h1 className="mt-20 ml-3 mr-3 flex justify-center max-w-md text-xl">{data.quote}</h1>
-                                <h2 className="mt-4 ml-3 font-semibold">&nbsp;-&nbsp;{data.name}</h2>
+                                </div>
+                                <div>
+                                    <h1 className="mt-20 ml-3 mr-3 flex justify-center max-w-md text-xl">{data.quote}</h1>
+                                    <h2 className="mt-4 ml-3 font-semibold">&nbsp;-&nbsp;{data.name}</h2>
+                                </div>
                             </div>
                         </div>                   
                     </div>
                 </div>
-            </div>
-            <div className="flex justify-center">
-                <div className="max-w-36 px-20 lg:px-0 lg:max-w-xs lg:max-h-96 lg:z-20 lg:transform lg:-translate-x-12">
+                <div className="flex justify-center">
+                    <div className="max-w-36 px-20 lg:px-0 lg:max-w-xs lg:max-h-96 lg:z-20 lg:transform lg:-translate-x-12">
                         <img
                         className="lg:rounded-md lg:h-full object-cover flex justify-center items-center"
                             src={data.image}
                             alt={data.alt}
                         />
+                    </div>
                 </div>
             </div>
-            </div>
-            {/* </div> */}
         </div>
     )
 }
