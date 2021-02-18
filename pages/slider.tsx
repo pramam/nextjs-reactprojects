7592eb89c69a333a719cca104bc2f187b1ab9f4e
pramam <pallavi.ramam@gmail.com>
2021-02-18 15:22:20 -0800

    Slider:Remove bg-red color and make it dark gray

--- pages/slider.tsx
+++ pages/slider.tsx
@@ -2,12 +2,11 @@ import Slider from '../components/9-Slider/Slider'
 
 export default function DisplaySlider() {
     return (
-        // bg-gray-900 
-        <div className="relative min-h-screen bg-red-900">
+        <div className="relative min-h-screen bg-gray-900">
             <div className="flex flex-col justify-center ">
                 <Slider />
                 <footer className="flex justify-center text-center">
-                    <div className="absolute text-gray-600 bottom-0">Icons made by <a href="https://www.freepik.com" title="Freepik"> &nbsp; Freepik &nbsp; </a> from &nbsp;<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                    <div className="absolute text-gray-500 bottom-0">Icons made by<a href="https://www.freepik.com" title="Freepik">&nbsp;Freepik&nbsp;</a>from&nbsp;<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
                 </footer>
             </div>
         </div>
