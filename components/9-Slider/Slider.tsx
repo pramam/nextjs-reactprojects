a7221128bcd6e72816a4c2211677d9cdb4cfdd25
pramam <pallavi.ramam@gmail.com>
2021-02-18 22:31:35 -0800

    Slider: Reduce Red real estate to match Green real estate.

--- components/9-Slider/Slider.tsx
+++ components/9-Slider/Slider.tsx
@@ -87,7 +87,9 @@ export default function Slider() {
         // <=md: Reduced h-200 to h-180 since the font sizes were reduced.
         // New Calculations:
         // Eye balling these heights for current length of quotes.
-        <div className="relative bg-red-900 h-168 md:h-180 lg:h-152 overflow-hidden">
+        // .map height: bg-green-900 h-156 md:h-168
+        // TODO-HEIGHT: Change this height if quotes become too long
+        <div className="relative bg-red-900 h-156 md:h-168 lg:h-152 overflow-hidden">
             <div className="flex justify-center">
                 <div className="lg:mt-32">
                     <div className="flex flex-row justify-center">
@@ -110,6 +112,7 @@ export default function Slider() {
                                 {/* New calculations for entire Slider component: h-168 md:h-180 lg:h-152 */}
                                 {/* New calculations for entire map component: */}
                                 {/* <div className="bg-green-900 h-200 lg:h-140 lg:top-0 w-56 md:w-128 lg:w-224"> */}
+                                {/* TODO-HEIGHT: Change this height if quotes become too long */}
                                 <div className="bg-green-900 h-156 md:h-168 lg:h-140 lg:top-0 w-56 md:w-128 lg:w-224">
                                    {quoteData.map((obj, index) => {
                                     // All the height and width props are outside of the map
