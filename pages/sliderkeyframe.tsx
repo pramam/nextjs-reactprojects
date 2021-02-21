c27c449324fb59c3ba58890acdb00a09733643b9
pramam <pallavi.ramam@gmail.com>
2021-02-20 23:52:30 -0800

    SliderKeyframe:Got the frames to line up next to each other in a row

---
+++ pages/sliderkeyframe.tsx
@@ -0,0 +1,15 @@
+import quoteDataGeneric from '../components/9-Slider/SliderGeneric/sliderdatageneric.json'
+import SliderKeyframe from '../components/9-Slider/SliderGeneric/SliderKeyframe'
+
+export default function DisplaySlider() {
+    return (
+        <div className="relative min-h-screen bg-gray-900">
+            <div className="flex flex-col justify-center ">
+                <SliderKeyframe slides={quoteDataGeneric} />
+                <footer className="flex justify-center text-center">
+                    <div className="absolute text-gray-500 bottom-0">Icons made by<a href="https://www.freepik.com" title="Freepik">&nbsp;Freepik&nbsp;</a>from&nbsp;<a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                </footer>
+            </div>
+        </div>
+    )
+}
