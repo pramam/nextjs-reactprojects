348832cafe7b9d297ef9440d007eb9fed1d52096
pramam <pallavi.ramam@gmail.com>
2021-02-26 16:47:24 -0800

    Lorem Ipsum: Initial checkin with basic form in tailwindcss; responsive

---
+++ components/11-LoremIpsum/LoremIpsum.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import loremipsumdata from './loremipsumdata.json'
+
+// Data obtained from: https://hipsum.co/?paras=8&type=hipster-latin
+export default function LoremIpsum() {
+    const [paragraphs, setParagraphs] = useState(0);
+
+    return (
+        <div className="flex justify-center">
+            <div className="flex flex-col">
+                <h1 className="text-center uppercase text-2xl mt-10 lg:text-3xl lg:mt-16">Tired of boring Lorem Ipsum?</h1>
+                <div className="mt-7">
+                    <div className="flex flex-row justify-center">
+                        <label htmlFor="paragraphs"
+                            className="text-2xl mr-3 align-baseline mt-2"> Paragraphs: </label>
+                        <form className="">
+                            <input
+                                type='number'
+                                id='paragraphs'
+                                name='paragraphs'
+                                value={paragraphs}
+                                onChange={(e) => setParagraphs(e.target.valueAsNumber)}
+                                className="w-16 mr-2">
+                            </input>
+                            <button
+                                type="button"
+                                className="py-1 px-2 lg:py-2 lg:px-3 lg:mx-3 bg-gray-500 text-gray-900 uppercase font-bold rounded-md border border-transparent focus:outline-none">
+                                Generate
+                        </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ pages/loremipsum.tsx
@@ -0,0 +1,9 @@
+import LoremIpsum from '../components/11-LoremIpsum/LoremIpsum'
+
+export default function DisplayLoremIpsum() {
+    return (
+        <div className="min-h-screen bg-gradient-to-r from-gray-300 via-green-300 to-gray-300">
+            <LoremIpsum />
+        </div>
+    )
+}
