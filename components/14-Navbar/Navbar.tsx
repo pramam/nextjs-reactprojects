608caac0854d2ffbbf1c08ef462445085202f654
pramam <pallavi.ramam@gmail.com>
2021-03-04 14:37:27 -0800

    Navbar Project: Show dropdown on clicking menu using useState

--- components/14-Navbar/Navbar.tsx
+++ components/14-Navbar/Navbar.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import { navlinks, sociallinks } from './navbaritems'
 import SVGMenu from '../svgicons/SVGMenu'
 
 export default function Navbar() {
+    const [isopen, setIsOpen] = useState(false)
+
+    const clickHandler = () => {
+        setIsOpen(!isopen)
+    }
+
     return (
         <div className="bg-white mt-5">
+            <div className="flex flex-col">
             <div className="flex flex-row justify-between">
                 <div className="ml-2 sm:ml-5 md:ml-10 lg:ml-20 mb-5 text-3xl flex flex-row">
                     <div className="hidden md:block text-gray-900 font-semibold">Your&nbsp;</div>
@@ -29,7 +37,29 @@ export default function Navbar() {
                     </ul>
                 </div>
                 <div className="sm:hidden mr-2">
+                        <button type="button" onClick={clickHandler}>
                     <SVGMenu css="h-10 w-10 text-gray-400" />
+                        </button>
+                    </div>
+                </div>
+                {/* Drop down menu  */}
+                <div className="sm:hidden flex flex-col">
+                    {isopen ? (<ul className="flex flex-col mt-3">
+                        {navlinks.map((obj, index) => {
+                            return (
+                                <li key={index} className="mt-2">{obj.label}</li>
+                            )
+                        })}
+                    </ul>) : ''}
+                </div>
+                <div className="mt-3 mb-3 sm:hidden flex flex-col">
+                    {isopen ? (<ul className="flex flex-row mr-3">
+                        {sociallinks.map((obj, index) => {
+                            return (
+                                <li key={index} className="mr-2 text-blue-500">{obj.icon}</li>
+                            )
+                        })}
+                    </ul>) : ''}
                 </div>
             </div>
         </div>
