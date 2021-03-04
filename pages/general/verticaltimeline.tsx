9ffbc501added72f98b77d4bbbc13b060e662b12
pramam <pallavi.ramam@gmail.com>
2021-03-04 13:04:43 -0800

    General: Tailwind component for vertical timeline copied from tailwindcomponents.com

---
+++ pages/general/verticaltimeline.tsx
@@ -0,0 +1,9 @@
+import VerticalTimeline from '../../components/projects/VerticalTimeline'
+
+export default function DisplayVerticalTimeline() {
+    return (
+        <div>
+            <VerticalTimeline />
+        </div>
+    )
+}
