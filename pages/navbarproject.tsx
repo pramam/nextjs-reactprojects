c3424ae4783235f93bacbf8f38ae4761361f26af
pramam <pallavi.ramam@gmail.com>
2021-03-05 20:23:10 -0800

    Navbar Project: Add a \'projects\' navbar on top of the Navbar project

--- pages/navbarproject.tsx
+++ pages/navbarproject.tsx
@@ -1,8 +1,13 @@
 import Navbar from "../components/14-Navbar/Navbar";
+import ProjectNavbarContainerSimple from '../components/projects/ProjectNavbarContainerSimple'
 
 export default function NavbarProject() {
     return (
         <div className="min-h-screen bg-gradient-to-r from-gray-100 via-gray-100 to-gray-100">
+            <ProjectNavbarContainerSimple
+                projectName="Navbar"
+                textColor="text-gray-900"
+            />
             <Navbar />
         </div>
     )
