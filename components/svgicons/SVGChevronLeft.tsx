1193b48d5733eb8cc87bc7f400c8600721dc6d53
pramam <pallavi.ramam@gmail.com>
2021-03-07 11:28:28 -0800

    General: Change fill to none for L/R SVGicons

--- components/svgicons/SVGChevronLeft.tsx
+++ components/svgicons/SVGChevronLeft.tsx
@@ -10,7 +10,7 @@ export default function SVGChevronLeft({ css }: IProps) {
         <svg
             {...props}
             xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
+            fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor">
             <path
--- components/svgicons/SVGChevronRight.tsx
+++ components/svgicons/SVGChevronRight.tsx
@@ -11,7 +11,7 @@ export default function SVGChevronRight({ css }: IProps): JSX.Element {
         <svg
             {...props}
             xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
+            fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
         >
