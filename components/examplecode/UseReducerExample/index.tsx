d8ec3cdcb81c60eba2fedf5a9e37e63e585c16dc
pramam <pallavi.ramam@gmail.com>
2021-03-08 16:36:08 -0800

    Example Code: useReducer: useReducer with typescript

--- components/examplecode/UseReducerExample/index.tsx
+++ components/examplecode/UseReducerExample/index.tsx
@@ -1,38 +1,73 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer } from 'react'
 import Modal from './Modal'
 
+type State = {
+    people: {
+        id: number,
+        name: string
+    }[]
+    isModalOpen: boolean
+    modalContent: string
+}
+type Action =
+    | { type: 'TESTING' }
+// state right before the update, and the action
+const reducer = (state: State, action: Action): State => {
+    console.log(state, action)
+    return {
+        people: [],
+        isModalOpen: true,
+        modalContent: "testng"
+    }
+}
+
+const defaultState = {
+    people: [],
+    isModalOpen: false,
+    modalContent: 'hello world'
+}
 export default function Index() {
     const [name, setName] = useState('')
-    const [people, setPeople] = useState([])
-    const [showModal, setShowModal] = useState(false)
-    const [message, setMessage] = useState('')
+    const [state, dispatch] = useReducer(reducer, defaultState)
 
-    useEffect(() => {
-        const timeout_id = setTimeout(() => {
-            setMessage('')
-        }, 3000)
-        return (() => clearTimeout(timeout_id))
-    }, [message])
+    // dispatch is going to go through the reducer
+    // reducer takes in old state, action and spits out a new state
+    // useReducer is looking for reducer which is the function that is manipulating the state,
+    // and it will happen when dipatch the action
+
+    // const [people, setPeople] = useState([])
+    // const [showModal, setShowModal] = useState(false)
+    // const [message, setMessage] = useState('')
+
+    // useEffect(() => {
+    //     const timeout_id = setTimeout(() => {
+    //         setMessage('')
+    //     }, 3000)
+    //     return (() => clearTimeout(timeout_id))
+    // }, [message])
     const submitHandler = (e) => {
         e.preventDefault();
         if (name) {
-            setShowModal(true)
-            setPeople((oldPeople) => {
-                return ([...oldPeople, { id: new Date().getTime().toString(), name }])
-            })
-            setMessage("Item Added")
-            setName('')
-            console.log(`After setting people: ${people}`)
+            // setShowModal(true)
+            // setPeople((oldPeople) => {
+            //     return ([...oldPeople, { id: new Date().getTime().toString(), name }])
+            // })
+            // setMessage("Item Added")
+            // setName('')
+            // console.log(`After setting people: ${people}`)
+
+            // once you dispatch your action, you need to handle it in reducer
+            dispatch({ type: "TESTING" })
         }
         else {
-            setShowModal(true)
-            setMessage("Please enter value")
+            // setShowModal(true)
+            // setMessage("Please enter value")
         }
     }
     return (
         <div className="mt-6 mb-6 flex justify-center mx-auto shadow-lg max-w-2xl">
             <div className="flex flex-col">
-                {showModal && <Modal message={message} />}
+                {state.isModalOpen && <Modal message={state.modalContent} />}
                 <form onSubmit={submitHandler} className="mb-4">
                     <h1 className="mt-1 mb-4 text-xl text-center"> useReducer Example</h1>
                     <input type="text"
@@ -45,8 +80,7 @@ export default function Index() {
                         className="ml-2 p-2 bg-indigo-500 text-white rounded-md focus:outline-none">
                         Add Person</button>
                 </form>
-                {/* <p className="text-xl text-center"> </p> */}
-                {people.map((obj, index) => (
+                {state.people.map((obj, index) => (
                     <p key={obj.id} className="text-center"> {obj.name}</p>
                 ))}
             </div>
