709b9c6a14d4943259d3dd0d8ca3c76f5d25314b
pramam <pallavi.ramam@gmail.com>
2021-03-10 15:30:41 -0800

    Sidebar Modal: Add gray background and z-10 to Modal

--- components/16-SidebarModal/index.tsx
+++ components/16-SidebarModal/index.tsx
@@ -18,14 +18,15 @@ export default function SidebarModal() {
         <>
             <div className="flex flex-row">
                 {menuIsOpen ?
-                <Sidebar menuIsOpen={menuIsOpen} closeHandler={menuHandler} />
+                    <Sidebar menuIsOpen={menuIsOpen} closeHandler={menuHandler} />
                     :
                     <Menu clickHandler={menuHandler} />
                 }
             </div>
 
             {showModal ?
-                <div className="flex justify-center">
+                <div className="bg-gray-600 z-10 min-h-screen ">
+                    <div className="flex justify-center z-20">
                     <div className="relative w-4/5 sm:w-3/5 lg:w-1/2 2xl:w-1/5 mt-32 sm:mt-32 md:mt-40 shadow-lg bg-gray-50 rounded-md">
                         <div className="flex flex-row justify-end">
                             <p className="flex justify-center py-20 sm:py-28 lg:py-40 mx-auto text-3xl">Modal Content</p>
@@ -37,6 +38,7 @@ export default function SidebarModal() {
                             </div>
                         </div>
                     </div>
+                </div>
                 </div>
                 : 
             <div className="mx-auto text-center h-full">
