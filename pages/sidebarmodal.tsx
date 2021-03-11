4f65f26293e0b535963753128ccccf532a87cafa
pramam <pallavi.ramam@gmail.com>
2021-03-10 21:36:20 -0800

    Sidebar Modal: Add a 'projects' navbar on top of the project

--- pages/sidebarmodal.tsx
+++ pages/sidebarmodal.tsx
@@ -1,8 +1,13 @@
 import SidebarModal from '../components/16-SidebarModal'
+import ProjectNavbarContainerSimple from '../components/projects/ProjectNavbarContainerSimple'
 
 export default function DisplaySidebarModal() {
     return (
         <div className="min-h-screen bg-gradient-to-r from-green-500 via-gray-400 to-green-500 ">
+            <ProjectNavbarContainerSimple
+                projectName="Sidebar Modal"
+                textColor="text-gray-900"
+            />
             <SidebarModal />
         </div>
     )
