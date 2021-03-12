af1f5c2de1053cc707ad23e49baf5720af432b3b
pramam <pallavi.ramam@gmail.com>
2021-03-11 22:38:51 -0800

    General: Vertical Timeline Orig2: Added L and R cards

--- components/projects/VerticalTimeline/VerticalTimelineOrig2.tsx
+++ components/projects/VerticalTimeline/VerticalTimelineOrig2.tsx
@@ -1,3 +1,6 @@
+import LeftCardOrig2 from './LeftCardOrig2'
+import RightCardOrig2 from './RightCardOrig2'
+
 export default function VerticalTimelineOrig2() {
     return (
         <>
@@ -5,7 +8,7 @@ export default function VerticalTimelineOrig2() {
             <div className="container">
                 <div className="flex flex-col md:grid grid-cols-9 mx-auto p-2 text-blue-50">
                     {/* left Red */}
-                    <div className="flex flex-row-reverse md:contents">
+                    {/* <div className="flex flex-row-reverse md:contents">
                         <div className="bg-red-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
                             <h3 className="font-semibold text-lg mb-1">Lorem ipsum</h3>
                             <p className="leading-tight text-justify">
@@ -19,10 +22,10 @@ export default function VerticalTimelineOrig2() {
                             </div>
                             <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-red-500 shadow" />
                         </div>
-                    </div>
-
+                    </div> */}
+                    <LeftCardOrig2 />
                     {/* left Red */}
-                    <div className="flex flex-row-reverse md:contents">
+                    {/* <div className="flex flex-row-reverse md:contents">
                         <div className="bg-red-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
                             <h3 className="font-semibold text-lg mb-1">Lorem ipsum</h3>
                             <p className="leading-tight text-justify">
@@ -36,9 +39,10 @@ export default function VerticalTimelineOrig2() {
                             </div>
                             <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-red-500 shadow" />
                         </div>
-                    </div>
+                    </div> */}
+                    <LeftCardOrig2 />
                     {/* right Blue*/}
-                    <div className="flex md:contents">
+                    {/* <div className="flex md:contents">
                         <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-blue-800 pointer-events-none" />
@@ -52,9 +56,10 @@ export default function VerticalTimelineOrig2() {
                                 Vitae, facilis.
         </p>
                         </div>
-                    </div>
+                    </div> */}
+                    <RightCardOrig2 />
                     {/* left Green */}
-                    <div className="flex flex-row-reverse md:contents">
+                    {/* <div className="flex flex-row-reverse md:contents">
                         <div className="bg-green-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
                             <h3 className="font-semibold text-lg mb-1">Lorem ipsum</h3>
                             <p className="leading-tight text-justify">
@@ -68,7 +73,8 @@ export default function VerticalTimelineOrig2() {
                             </div>
                             <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-green-500 shadow" />
                         </div>
-                    </div>
+                    </div> */}
+                    <LeftCardOrig2 />
                     {/* left Yellow*/}
                     <div className="flex flex-row-reverse md:contents">
                         <div className="bg-yellow-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
@@ -86,7 +92,7 @@ export default function VerticalTimelineOrig2() {
                         </div>
                     </div>
                     {/* right Pink */}
-                    <div className="flex md:contents">
+                    {/* <div className="flex md:contents">
                         <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
                             <div className="h-full w-6 flex items-center justify-center">
                                 <div className="h-full w-1 bg-pink-800 pointer-events-none" />
@@ -100,8 +106,8 @@ export default function VerticalTimelineOrig2() {
                                 Vitae, facilis.
         </p>
                         </div>
-                    </div>
-
+                    </div> */}
+                    <RightCardOrig2 />
                     {/* left Red */}
                     <div className="flex flex-row-reverse md:contents">
                         <div className="bg-red-500 col-start-1 col-end-5 p-4 rounded-xl my-4 ml-auto shadow-md">
