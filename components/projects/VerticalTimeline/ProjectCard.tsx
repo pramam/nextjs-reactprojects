926f09d105e2b1afbff9c1e641daa4e09088aebf
pramam <pallavi.ramam@gmail.com>
2021-03-12 09:50:17 -0800

    General: Vertical Timeline: Fix the original bug by adding flex-wrapto the demo, etc. links; Change image display parameters

--- components/projects/VerticalTimeline/ProjectCard.tsx
+++ components/projects/VerticalTimeline/ProjectCard.tsx
@@ -13,15 +13,16 @@ export default function ProjectCard({ index, image, title, demo, blog, inspirati
     const textgray = "text-sm text-gray-300 uppercase mr-3 cursor-not-allowed"
     return (
         <div className="mt-2 flex flex-col flex-shrink shadow-xl mx-4">
-            {/* <div className="max-w-28">
+            <div className="max-w-4/6 sm:max-w-4/5">
                 <img
-                    className="h-64 w-70 object-cover"
+                    className="w-full h-64 object-cover flex justify-center items-center"
                     src={image}
-                    alt={title} />
-            </div> */}
+                    alt={title}
+                />
+            </div>
             <div className="bg-gray-50">
                 <div className="m-3 flex justify-center text-xl font-semibold text-gray-400 capitalize">{index}. {title}</div>
-                <div className="bg-gray-50 flex flex-row justify-center">
+                <div className="bg-gray-50 flex flex-row flex-wrap justify-center">
                     <a href={demo} className={demo ? text : textgray} >Demo</a>
                     <a href={blog} className={blog ? text : textgray}>Blog</a>
                     <a href={inspiration} className={inspiration ? text : textgray}>Inspiration</a>
