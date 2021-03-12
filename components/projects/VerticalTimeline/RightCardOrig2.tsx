8bba884f3d6e9d27136718ab53afe1f34cd18c65
pramam <pallavi.ramam@gmail.com>
2021-03-11 22:58:14 -0800

    General: Vertical Timeline Orig2: Added RightCardOrig2 and cleaned up code

--- components/projects/VerticalTimeline/RightCardOrig2.tsx
+++ components/projects/VerticalTimeline/RightCardOrig2.tsx
@@ -1,4 +1,6 @@
-export default function RightCardOrig2() {
+export default function RightCardOrig2({ demo, blog, inspiration, github }) {
+    const text = "text-sm text-gray-400 uppercase mr-3"
+    const textgray = "text-sm text-gray-300 uppercase mr-3 cursor-not-allowed"
     return (
         <div className="flex md:contents">
             <div className="col-start-5 col-end-6 mr-10 md:mx-auto relative">
@@ -13,6 +15,13 @@ export default function RightCardOrig2() {
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                     Vitae, facilis.
         </p>
+                <div className="m-3 flex justify-center text-xl font-semibold text-gray-400 capitalize">index. title</div>
+                <div className="bg-gray-50 flex flex-row flex-wrap justify-center">
+                    <a href="" className={demo ? text : textgray} >Demo</a>
+                    <a href="" className={blog ? text : textgray}>Blog</a>
+                    <a href="" className={inspiration ? text : textgray}>Inspiration</a>
+                    <a href="" className={github ? text : textgray}>Github</a>
+                </div>
             </div>
         </div>
     )
