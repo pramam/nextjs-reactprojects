456eac77afb7402602cf322d07b35160a16ac8e3
pramam <pallavi.ramam@gmail.com>
2021-03-11 16:20:54 -0800

    General: Vertical Timeline: Iterate through all projects

--- components/projects/VerticalTimeline/VerticalTimeline.tsx
+++ components/projects/VerticalTimeline/VerticalTimeline.tsx
@@ -13,56 +13,40 @@ export default function VerticalTimeline() {
                     <CenterTextCard 
                         title="Introduction"
                         blog="" />
-                    <LeftCard
-                        index={1}
-                        image={allprojectsData[0].image}
-                        title={allprojectsData[0].name}
-                        day={allprojectsData[0].day}
-                        demo={allprojectsData[0].url}
-                        blog={allprojectsData[0].blogurl}
-                        inspiration={allprojectsData[0].inspiration}
-                        github={allprojectsData[0].githuburl}
-                    />
-                    <RightCard
-                        index={2}
-                        image={allprojectsData[1].image}
-                        title={allprojectsData[1].name}
-                        day={allprojectsData[1].day}
-                        demo={allprojectsData[1].url}
-                        blog={allprojectsData[1].blogurl}
-                        inspiration={allprojectsData[1].inspiration}
-                        github={allprojectsData[1].githuburl}
-                    />
-                    <LeftCard
-                        index={3}
-                        image={allprojectsData[2].image}
-                        title={allprojectsData[2].name}
-                        day={allprojectsData[2].day}
-                        demo={allprojectsData[2].url}
-                        blog={allprojectsData[2].blogurl}
-                        inspiration={allprojectsData[2].inspiration}
-                        github={allprojectsData[2].githuburl}
-                    />
-                    <RightCard
-                        index={4}
-                        image={allprojectsData[3].image}
-                        title={allprojectsData[3].name}
-                        day={allprojectsData[3].day}
-                        demo={allprojectsData[3].url}
-                        blog={allprojectsData[3].blogurl}
-                        inspiration={allprojectsData[3].inspiration}
-                        github={allprojectsData[3].githuburl}
-                    />
-                    <LeftCard
-                        index={5}
-                        image={allprojectsData[4].image}
-                        title={allprojectsData[4].name}
-                        day={allprojectsData[4].day}
-                        demo={allprojectsData[4].url}
-                        blog={allprojectsData[4].blogurl}
-                        inspiration={allprojectsData[4].inspiration}
-                        github={allprojectsData[4].githuburl}
-                    />
+                    {allprojectsData.map((obj, index) => {
+
+
+                        return (
+                            <>
+                                {index % 2 === 0 ?
+                                    <LeftCard
+                                        index={index + 1}
+                                        image={obj.image}
+                                        title={obj.name}
+                                        day={obj.day}
+                                        demo={obj.url}
+                                        blog={obj.blogurl}
+                                        inspiration={obj.inspiration}
+                                        github={obj.githuburl}
+                                    /> :
+                                    <RightCard
+                                        index={index + 1}
+                                        image={obj.image}
+                                        title={obj.name}
+                                        day={obj.day}
+                                        demo={obj.url}
+                                        blog={obj.blogurl}
+                                        inspiration={obj.inspiration}
+                                        github={obj.githuburl}
+                                    />
+
+                                }
+                            </>
+
+                        )
+                    }
+
+                    )}
                 </div>
             </div>
         </>
