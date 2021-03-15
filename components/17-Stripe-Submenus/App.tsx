ee8fca51b2cea735e166b2a61182efcf90775a19
pramam <pallavi.ramam@gmail.com>
2021-03-14 20:24:36 -0700

    17: Stripe Submenus: Add scale transform <Transition> to Sidebar

--- components/17-Stripe-Submenus/App.tsx
+++ components/17-Stripe-Submenus/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 import Footer from './Footer'
 import { useGlobalStripeContext } from './Context'
+import { Transition } from '@headlessui/react'
 
 export default function App() {
     const { isSidebarOpen } = useGlobalStripeContext()
@@ -12,8 +13,19 @@ export default function App() {
         <Hero >
             {/* Sidebar, when open, will cover the entire screen hiding Navbar and Home behind it  */}
             {/* Sidebar's position is fixed, so it covers Home/Navbar */}
-            {isSidebarOpen ?
-                <Sidebar /> : ``}
+            <Transition
+                show={isSidebarOpen}
+                enter="transform duration-700"
+                enterFrom="scale-0 "
+                enterTo="scale-100"
+                leave="transform duration-700"
+                leaveFrom="scale-100"
+                leaveTo="scale-0"
+            >
+                <Sidebar />
+            </Transition>
+            {/* {isSidebarOpen ?
+                <Sidebar /> : ``} */}
             <Navbar />
             <Home />
         </Hero>
