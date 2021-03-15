78f6c4781e2783c22edd7c271e2d8e0eca5b3a23
pramam <pallavi.ramam@gmail.com>
2021-03-14 20:01:21 -0700

    17: Stripe Submenus: Fix JSX in SVG Icons for stripe

--- components/17-Stripe-Submenus/svgimages/SVGHero.tsx
+++ components/17-Stripe-Submenus/svgimages/SVGHero.tsx
@@ -1,34 +1,89 @@
-export default function SVGHero() {
+export default function SVGHero(props) {
     return (
-        <svg width="1347" height="528" viewBox="0 0 1347 528" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1346.19 238.903L0.624512 527.476V0.157471H1346.19V238.903Z" fill="url(#paint0_linear)" />
-            <path d="M1346.19 238.903L0.624512 527.476V0.157471H1346.19V238.903Z" fill="url(#paint1_radial)" />
-            <path d="M1346.19 238.903L0.624512 527.476V0.157471H1346.19V238.903Z" fill="url(#paint2_radial)" />
-            <path d="M1346.19 238.903L0.624512 527.476V0.157471H1346.19V238.903Z" fill="url(#paint3_radial)" />
-            <path d="M1346.19 238.903L0.624512 527.476V0.157471H1346.19V238.903Z" fill="url(#paint4_radial)" />
-            <defs>
-                <linearGradient id="paint0_linear" x1="578.124" y1="281.062" x2="673.409" y2="527.476" gradientUnits="userSpaceOnUse">
-                    <stop stop-color="#7BE1FF" />
-                    <stop offset="1" stop-color="#C4C4C4" stop-opacity="0" />
-                </linearGradient>
-                <radialGradient id="paint1_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(575.385 168.744) rotate(74.7169) scale(371.884 948.943)">
-                    <stop stop-color="#EBC471" />
-                    <stop offset="1" stop-color="white" stop-opacity="0" />
-                </radialGradient>
-                <radialGradient id="paint2_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(1068.49 146.828) rotate(169.545) scale(936.004 2388.42)">
-                    <stop stop-color="#C200E8" />
-                    <stop offset="1" stop-color="white" stop-opacity="0" />
-                </radialGradient>
-                <radialGradient id="paint3_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(786.325 220.794) rotate(110.213) scale(326.809 833.924)">
-                    <stop stop-color="#8ECAF7" />
-                    <stop offset="1" stop-color="white" stop-opacity="0" />
-                </radialGradient>
-                <radialGradient id="paint4_radial" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(539.772 404.339) rotate(88.1323) scale(252.166 643.456)">
-                    <stop stop-color="#E2C67E" />
-                    <stop offset="1" stop-color="white" stop-opacity="0" />
-                </radialGradient>
-            </defs>
+        <svg
+            width={1347}
+            height={528}
+            viewBox="0 0 1347 528"
+            fill="none"
+            {...props}
+        >
+            <path
+                d="M1346.19 238.903L.625 527.476V.157H1346.19v238.746z"
+                fill="url(#paint0_linear)"
+            />
+            <path
+                d="M1346.19 238.903L.625 527.476V.157H1346.19v238.746z"
+                fill="url(#paint1_radial)"
+            />
+            <path
+                d="M1346.19 238.903L.625 527.476V.157H1346.19v238.746z"
+                fill="url(#paint2_radial)"
+            />
+            <path
+                d="M1346.19 238.903L.625 527.476V.157H1346.19v238.746z"
+                fill="url(#paint3_radial)"
+            />
+            <path
+                d="M1346.19 238.903L.625 527.476V.157H1346.19v238.746z"
+                fill="url(#paint4_radial)"
+            />
+                <defs>
+                <linearGradient
+                    id="paint0_linear"
+                    x1={578.124}
+                    y1={281.062}
+                    x2={673.409}
+                    y2={527.476}
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stopColor="#7BE1FF" />
+                    <stop offset={1} stopColor="#C4C4C4" stopOpacity={0} />
+                    </linearGradient>
+                <radialGradient
+                    id="paint1_radial"
+                    cx={0}
+                    cy={0}
+                    r={1}
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="rotate(74.717 177.172 461.224) scale(371.884 948.943)"
+                >
+                    <stop stopColor="#EBC471" />
+                    <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </radialGradient>
+                <radialGradient
+                    id="paint2_radial"
+                    cx={0}
+                    cy={0}
+                    r={1}
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="rotate(169.545 527.528 122.293) scale(936.004 2388.42)"
+                >
+                    <stop stopColor="#C200E8" />
+                    <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </radialGradient>
+                <radialGradient
+                    id="paint3_radial"
+                    cx={0}
+                    cy={0}
+                    r={1}
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="matrix(-112.91615 306.68235 -782.5665 -288.13002 786.325 220.794)"
+                >
+                    <stop stopColor="#8ECAF7" />
+                    <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </radialGradient>
+                <radialGradient
+                    id="paint4_radial"
+                    cx={0}
+                    cy={0}
+                    r={1}
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="matrix(8.21853 252.03204 -643.11416 20.97135 539.772 404.339)"
+                >
+                    <stop stopColor="#E2C67E" />
+                    <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                    </radialGradient>
+                </defs>
         </svg>
-
     )
 }
