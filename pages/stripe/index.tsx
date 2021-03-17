88846db526a2499cfc2f88586dc48b3c348a6d9a
pramam <pallavi.ramam@gmail.com>
2021-03-16 21:56:06 -0700

    17:Stripe Submenus: Add project to Vertical Timeline

--- pages/stripe/index.tsx
+++ pages/stripe/index.tsx
@@ -1,7 +1,15 @@
 import Stripe from '../../components/17-Stripe-Submenus'
+import ProjectNavbarContainerSimple from '../../components/projects/ProjectNavbarContainerSimple'
 
 export default function DisplayStripe() {
     return (
+        <div>
+            <ProjectNavbarContainerSimple
+                projectId={17}
+                textColor="text-gray-900"
+                includeBlogs={false}
+            />
         <Stripe />
+        </div>
     )
 }
