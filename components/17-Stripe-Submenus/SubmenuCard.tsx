426a173485d582f6ab4491260e9302512cc6703a
pramam <pallavi.ramam@gmail.com>
2021-03-16 17:57:01 -0700

    17: Stripe Submenus: Display data from datafile in SubmenuCard

--- components/17-Stripe-Submenus/SubmenuCard.tsx
+++ components/17-Stripe-Submenus/SubmenuCard.tsx
@@ -1,19 +1,34 @@
 import { useGlobalStripeContext } from './Context'
+import submenuData from './submenudata'
 
 export default function SubmenuCard() {
     const { navButtonToShow, navButtonIndex, navPrevButtonIndex, coordinates } = useGlobalStripeContext()
     const topSpacePx = 0 //-3
+    const links = submenuData[navButtonIndex].links;
 
     return (
         <div>
-            <div className="absolute py-10 bg-gray-50 z-10 inline-block rounded-md"
+            <div className="absolute py-7 bg-gray-50 z-10 inline-block rounded-md shadow-lg"
                 style={{ left: `${coordinates.left}px`, top: `${coordinates.bottom + topSpacePx}px` }}
             >
                 <div className="flex flex-col">
 
-                    <li>Submenu: {navButtonToShow}: index: {navButtonIndex}</li>
+                    <h3 className="ml-3 mb-3 font-semibold text-indigo-900 uppercase text-sm">{navButtonToShow}</h3>
+                    {/* <li>Submenu: {navButtonToShow}: index: {navButtonIndex}</li>
                     <li>PrevButtonIndex: {navPrevButtonIndex} NavButtonIndex: {navButtonIndex} </li>
-                    <li>Coords: {coordinates.center}, {coordinates.bottom}</li>
+                    <li>Coords: {coordinates.center}, {coordinates.bottom}</li> */}
+                    <ul className="ml-3 flex flex-row">
+                        {links.map((obj, index) => {
+                            return (
+                                <li key={index}>
+                                    <div className="flex flex-row">
+                                        {obj.icon({ css: `h-4 w-4 ${obj.textcolor} mt-1` })}
+                                        <div className="ml-3 mr-10">{obj.name}</div>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
                 </div>
             </div>
         </div>
