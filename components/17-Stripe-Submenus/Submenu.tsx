03b3706c063a05a53d61ba2d0a9ffbafb648a5cf
pramam <pallavi.ramam@gmail.com>
2021-03-16 22:15:10 -0700

    17: Stripe Submenus: Comment out <Transition> since it is not working properly

--- components/17-Stripe-Submenus/Submenu.tsx
+++ components/17-Stripe-Submenus/Submenu.tsx
@@ -17,39 +17,27 @@ export default function Submenu() {
         // ref={refContainer}
         >
             {isSubmenuOpen ?
-                <Transition
-                    // appear doesn't seem to do what I expect
-                    appear={false}
-                    // #SubmenuTransition
-                    show={navButtonIndex != navPrevButtonIndex}
-                    // enter="transform duration-700"
-                    // enterFrom="scale-0 "
-                    // enterTo="scale-100"
-                    // leave="transform duration-700"
-                    // leaveFrom="scale-100"
-                    // leaveTo="scale-0"
+                // <Transition
+                //     // appear doesn't seem to do what I expect
+                //     appear={false}
+                //     // #SubmenuTransition
+                //     show={navButtonIndex != navPrevButtonIndex}
+                //     // enter="transform duration-700"
+                //     // enterFrom="scale-0 "
+                //     // enterTo="scale-100"
+                //     // leave="transform duration-700"
+                //     // leaveFrom="scale-100"
+                //     // leaveTo="scale-0"
 
-                    enter="transition ease-in-out duration-700 transform"
-                    enterFrom="translate-x-0" //"-translate-x-1/2"
-                    enterTo="translate-x-0"
-                    leave="transition ease-in-out duration-700 transform"
-                    leaveFrom="translate-x-0"
-                    leaveTo="-translate-x-1/2"
-                >
+                //     enter="transition ease-in-out duration-700 transform"
+                //     enterFrom="translate-x-0" //"-translate-x-1/2"
+                //     enterTo="translate-x-0"
+                //     leave="transition ease-in-out duration-700 transform"
+                //     leaveFrom="translate-x-0"
+                //     leaveTo="-translate-x-1/2"
+                // >
                     <SubmenuCard />
-                    {/* <div>
-                    <div className="absolute py-10 bg-gray-50 z-10 inline-block rounded-md"
-                        style={{ left: `${coords.left}px`, top: `${coords.bottom + topSpacePx}px` }}
-                    >
-                        <div className="flex flex-col">
-
-                            <li>Submenu: {navButtonToShow}: index: {navButtonIndex}</li>
-                            <li>PrevButtonIndex: {navPrevButtonIndex} NavButtonIndex: {navButtonIndex} </li>
-                            <li>Coords: {coords.center}, {coords.bottom}</li>
-                        </div>
-                    </div>
-                </div> */}
-                </Transition>
+                // </Transition>
                 : ``
             }
         </div>
