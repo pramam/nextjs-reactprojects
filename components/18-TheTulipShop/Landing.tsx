ea3159e4f1ed279a71aa320f4a1ad507a01d17cb
pramam <pallavi.ramam@gmail.com>
2021-03-18 20:47:13 -0700

    18: The Tulip Shop: Make the hero image and overlay text responsive

--- components/18-TheTulipShop/Landing.tsx
+++ components/18-TheTulipShop/Landing.tsx
@@ -3,25 +3,25 @@ import tulipphotos from './tulipphotos.json'
 export default function Landing() {
     return (
         <div>
-            <div className="flex items-center justify-center h-1/5">
-                <h1 className="font-tuliplogo text-yellow-50 text-center py-10 text-6xl"> The Tulip Shop</h1>
+            <div className="flex items-center justify-center h-1/4 lg:h-1/5">
+                <h1 className="font-tuliplogo text-yellow-50 text-center py-6 lg:py-10 text-4xl sm:text-5xl lg:text-6xl"> The Tulip Shop</h1>
             </div>
             {/* Hero Image  */}
             <div className="grid grid-cols-1 relative">
                 {/* Putting text over image: https://www.youtube.com/watch?v=Zo1JtXnLDdA  */}
                 <img
-                    className="absolute inset-0 object-cover w-full h-96"
+                    className="absolute inset-0 object-cover w-full h-32 sm:h-48 md:h-60 lg:h-96"
                     src={tulipphotos[0].img}
                     alt={tulipphotos[0].img}
                 />
-                <div className="absolute inset-0 bg-gray-900 bg-opacity-60"></div>
-                <div className="flex items-center justify-center relative transform -translate-y-5 h-96 text-center">
-                    <span className="border-4 justify-center border-yellow-50 px-20 py-8 text-yellow-50 uppercase text-center text-5xl tracking-wider">
+                <div className="absolute inset-0 bg-gray-900 bg-opacity-30 sm:bg-opacity-40 lg:bg-opacity-50"></div>
+                <div className="flex items-center justify-center relative transform -translate-y-2 sm:-translate-y-5 h-32 sm:h-48 md:h-60 lg:h-96 text-center">
+                    <span className="border-2 md:border-4 justify-center border-yellow-50 px-3 py-1 sm:px-6 sm:py-2 md:px-10 md:py-4 lg:px-20 lg:py-8 text-yellow-50 uppercase text-center text-xl sm:text-2xl md:text-3xl lg:text-5xl tracking-wider">
                         Bring Beauty Inside
                     </span>
                 </div>
-                <div className="flex items-center justify-center relative transform -translate-y-24 text-center">
-                    <span className="border justify-center border-yellow-50 px-8 py-2 text-yellow-50 text-lg uppercase text-center tracking-widest">Shop now</span>
+                <div className="flex items-center justify-center relative transform -translate-y-8 sm:-translate-y-16 md:-translate-y-20 lg:-translate-y-24 text-center">
+                    <span className="border justify-center border-yellow-50 px-2 py-1 sm:px-8 sm:py-2 text-yellow-50 text-sm sm:text-base md:text-lg uppercase text-center tracking-widest">Shop now</span>
                 </div>
             </div>
 
@@ -44,7 +44,7 @@ export default function Landing() {
                 Image 5, 6: lg: h-112
                 Image 7, 8: lg:h-72 */}
             <div className="flex items-center">
-                <div className="flex-1 max-w-4/6 mx-auto p-10">
+                <div className="flex-1 max-w-4/6 mx-auto sm:mt-6 lg:mt-10">
                     <ul className="grid grid-cols-12 gap-x-3 gap-y-4 grid-flow-row">
                         <li className="col-span-4 relative">
                             <img
