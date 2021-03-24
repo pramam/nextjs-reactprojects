553c4d956088ac283005f2e8ffe95a367e4c5271
pramam <pallavi.ramam@gmail.com>
2021-03-24 16:12:04 -0700

    18: The Tulip Shop: Display items with totals in Cart

--- components/18-TheTulipShop/Cart.tsx
+++ components/18-TheTulipShop/Cart.tsx
@@ -5,7 +5,7 @@ import ShopItemCounter from './ShopItemCounter'
 // This component displays the items from the inventory, stored in cartItems
 // For the amount to display, it fetches it from the global cartState
 export default function Cart() {
-    const { getItemsInCart, decrementCartItemCount, incrementCartItemCount } = useGlobalTulipContext()
+    const { getItemsInCart, totalCount, totalPrice, decrementCartItemCount, incrementCartItemCount } = useGlobalTulipContext()
 
     const incrHandler = (id) => {
         incrementCartItemCount(id)
@@ -16,29 +16,107 @@ export default function Cart() {
     return (
         <div className="flex items-center">
             <div className="flex-1 max-w-10/12 sm:max-w-10/12 lg:max-w-10/12 mx-auto pt-5 pb-10 sm:pt-4 sm:pb-8 lg:pt-5 lg:pb-10">
-                <div className="flex justify-center">Cart Landing</div>
-                <ul className="grid grid-cols-12 sm:gap-x-3 gap-y-6 sm:gap-y-8 lg:gap-y-8 grid-flow-row">
-                    {/* We are iterating over the inventory here and displaying all the Selections
-                    obj.quantity holds the number in your cart, reflected on the Selections page */}
+                {/* <div className="flex justify-center text-3xl py-10 font-semibold">
+                    {getItemsInCart().length === 0 ? `Your Cart Is Empty` : `Your Cart`}
+                </div> */}
+                {getItemsInCart().length === 0 ?
+                    <h2 className="flex justify-center text-3x py-10 font-semibold">Your Cart Is Empty</h2> :
+                    <>
+                        <h2 className="flex justify-center text-3x py-10 font-semibold">Your Cart</h2>
+                        <div className="max-w-full">
+                            <table className="table-fixed">
+                                <thead>
+                                    <tr>
+                                        <th className="w-1/5">Image</th>
+                                        <th className="w-1/5 ">Name</th>
+                                        <th className="w-1/5">Price</th>
+                                        <th className="w-1/5">Quantity</th>
+                                        <th className="w-1/5">Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {/* getItemsInCart returns positive counts in Cart. */}
+                                    {getItemsInCart().map((obj, index) => {
+                                        return (
+                                            <tr className="">
+                                                <td className="">
+                                                    <img
+                                                        className="bg-white object-cover h-32 w-32 sm:h-40 sm:w-40"
+                                                        src={obj.img}
+                                                        alt={obj.alt}
+                                                    />
+                                                </td>
+                                                <td>
+                                                    <div className="capitalize tracking-widest text-left ml-5">{obj.name}</div>
+                                                </td>
+                                                <td>
+                                                    <h3 className="text-center">${obj.price}</h3>
+                                                </td>
+                                                <td>
+                                                    <div className="text-center">
+                                                        <ShopItemCounter
+                                                            id={obj.id}
+                                                            incrHandler={incrHandler}
+                                                            decrHandler={decrHandler}
+                                                            // obj.quantity is the count of this item in the cart, in state,
+                                                            // as changed by the user
+                                                            count={obj.quantity}
+                                                        />
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div className="text-center">
+                                                        ${obj.quantity * obj.price}
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td className="uppercase font-semibold text-lg text-center">Totals</td>
+                                        <td className="text-center font-semibold text-lg">{totalCount}</td>
+                                        <td className="text-center font-semibold text-lg">${totalPrice}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </>
+                }
+                {/* <ul className="grid grid-cols-15 sm:gap-x-3 gap-y-6 sm:gap-y-8 lg:gap-y-8 grid-flow-row">
+
+                    <div className="grid grid-cols-15 grid-flow-col">
+                        <p className="col-span-3 text-left">Image</p>
+                        <p className="col-span-3 text-left">Name</p>
+                        <p className="col-span-3 text-left">Price</p>
+                        <p className="col-span-3 text-left">Quantity</p>
+                        <p className="col-span-3 text-left">Total</p>
+                    </div>
+
+
                     {getItemsInCart().map((obj, index) => {
-                        const all_props = "col-span-12 sm:col-span-6 md:col-span-4"
-                        const footer_props = `col-span-12`
+                        // const all_props = "col-span-12 sm:col-span-6 md:col-span-4"
+                        const all_props = "col-span-15"
+                        const footer_props = `col-span-15`
 
                         return (
                             <li key={index}
                                 className={`${obj.id === 16 ? footer_props : all_props}`}
                             >
-                                <div className="flex flex-col">
-                                    <img
-                                        className="bg-white object-cover h-88 sm:h-80 w-full"
-                                        src={obj.img}
-                                        alt={obj.alt}
-                                    />
-                                    <div className="text-yellow-50 flex items-center justify-center relative text-center">
-                                        <span className="justify-center py-2 capitalize text-center tracking-widest">{obj.name}</span>
+                                <div className="grid grid-cols-15 grid-flow-row">
+                                    <div className="grid grid-cols-15 grid-flow-col">
+                                        <div className="col-span-3">
+                                        <img
+                                                // className="bg-white object-cover h-88 sm:h-80 w-full"
+                                                className="bg-white object-cover h-40 w-40 sm:h-40 sm:w-40"
+                                            src={obj.img}
+                                            alt={obj.alt}
+                                            />
                                     </div>
-                                    <div className="flex flex-row justify-around">
-                                        <h3>${obj.price}</h3>
+                                        <div className="col-span-3 capitalize tracking-widest text-left">{obj.name}</div>
+                                        <h3 className="col-span-3 text-left">${obj.price}</h3>
+                                        <div className="col-span-3 text-left">
                                         <ShopItemCounter
                                             id={obj.id}
                                             incrHandler={incrHandler}
@@ -48,11 +126,15 @@ export default function Cart() {
                                             count={obj.quantity}
                                         />
                                     </div>
+                                        <div className="col-span-3 text-left">
+                                            ${obj.quantity * obj.price}
+                                        </div>
+                                    </div>
                                 </div>
                             </li>
                         )
                     })}
-                </ul>
+                </ul> */}
             </div>
         </div>
     )
