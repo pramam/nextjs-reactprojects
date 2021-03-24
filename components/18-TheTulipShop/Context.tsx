86ca2837dcd929700f49a92b06f81b97c359eebf
pramam <pallavi.ramam@gmail.com>
2021-03-24 16:55:42 -0700

    18: The Tulip Shop: Call the reducer GET_TOTALS in useEffect

--- components/18-TheTulipShop/Context.tsx
+++ components/18-TheTulipShop/Context.tsx
@@ -33,19 +33,26 @@ const TulipProvider = ({ children }) => {
         // once you dispatch your action, you need to handle it in reducer
         console.log(`decrementCountHandler: id: ${id}`)
         cartDispatch({ type: "DECREMENT_COUNT", payload: { id } })
-        getTotals()
+        // getTotals()
     }
 
     const incrementCartItemCount = (id) => {
         // once you dispatch your action, you need to handle it in reducer
         console.log(`incrementCartItemCount: id: ${id}`)
         cartDispatch({ type: "INCREMENT_COUNT", payload: { id } })
-        getTotals()
+        // getTotals()
     }
 
-    const getTotals = () => {
+    // It might be better to call getTotals in a useEffect every time the cart changes
+    // instead of finding all the places where the cart changes
+
+    useEffect(() => {
         cartDispatch({ type: "GET_TOTALS" })
-    }
+    }, [cartState.cartItems])
+
+    // const getTotals = () => {
+    //     cartDispatch({ type: "GET_TOTALS" })
+    // }
 
     // I do not want to 'reduce' the inventory. I just want to filter out what I need,
     // so it looks like this cannot be done through the dispatcher as the returned state updates
@@ -65,7 +72,7 @@ const TulipProvider = ({ children }) => {
                 ...cartState,// cartItems gets put in the context
                 decrementCartItemCount,
                 incrementCartItemCount,
-                getTotals,
+                // getTotals,
                 getItemsInCart
             }}>
             {children}
