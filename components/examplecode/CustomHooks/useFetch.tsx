76f46c72487568509c9e60b67074653369bb68ff
pramam <pallavi.ramam@gmail.com>
2021-03-27 15:24:22 -0700

    Example Code: Add useCallback to useFetch custom hook

--- components/examplecode/CustomHooks/useFetch.tsx
+++ components/examplecode/CustomHooks/useFetch.tsx
@@ -5,14 +5,27 @@ export const useFetch = (url: string) => {
     const [loading, setLoading] = useState(true)
     const [products, setProducts] = useState([])
 
-    const getProducts = async () => {
+    // Previously this would change every single time the state variable products 
+    // changed, so we couldn't add getProducts to useEffect
+    // const getProducts = async () => {
+    //     const response = await fetch(url)
+    //     const products = await response.json()
+    //     setProducts(products)
+    //     setLoading(false)
+    // }
+
+    // now we only change getProducts when url changes
+    const getProducts = useCallback(async () => {
         const response = await fetch(url)
         const products = await response.json()
         setProducts(products)
         setLoading(false)
-    }
+    }, [url])
+
+    // so now I can add getProducts to the dependency array as it changes only
+    // when url changes. It will not result in an infinite loop.
     useEffect(() => {
         getProducts()
-    }, [url]) // if I add products here in the dependency array I will get an infinite loop
+    }, [url, getProducts]) // if I add getProducts here in the dependency array I will get an infinite loop
     return { loading, products }
 }
