e04bc7f6830cb66c64cb96d05b5525d0fb8f6185
pramam <pallavi.ramam@gmail.com>
2021-03-30 14:24:59 -0700

    Example Code: Equal height text cards with text also aligned using flex-1

--- components/examplecode/EqualCards/index.tsx
+++ components/examplecode/EqualCards/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
         <div className="flex">
             <ul className="flex flex-row">
                 {data.map((obj, index) => {
-                    const common_props = "flex-1 border-2 border-red-700"
+                    const common_props = "flex flex-col flex-1 border-2 border-red-700"
                     return (
                         <li key={index}
                             className={index % 2 ? `${common_props} bg-gray-300` : `${common_props} bg-blue-300`}>
@@ -13,10 +13,10 @@ export default function Index() {
                             <img
                                 className="rounded-t-lg sm:w-full sm:object-cover sm:h-64 md:h-64 2xl:h-76"
                                 src={obj.image} alt="" />
-
-                            <p className="border border-pink-500 bg-white">{obj.text}</p>
-                            {/* <p>Details</p> */}
-
+                            <div className="flex flex-col flex-1">
+                                <p className="flex-1 border border-pink-500 bg-white">{obj.text}</p>
+                                <p className="">Details</p>
+                            </div>
                         </li>
                     )
                 })}
