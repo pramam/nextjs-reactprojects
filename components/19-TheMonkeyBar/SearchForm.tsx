29b3f83a410d33284d0a73dc740c433110e54308
pramam <pallavi.ramam@gmail.com>
2021-04-01 22:01:07 -0700

    19: The Monkey Bar: Put Search icon inside the Search bar

--- components/19-TheMonkeyBar/SearchForm.tsx
+++ components/19-TheMonkeyBar/SearchForm.tsx
@@ -7,19 +7,22 @@ export default function SearchForm() {
     return (
         <div className="mt-10 mx-auto max-w-md md:max-w-2xl shadow-lg flex justify-center">
             <div className="flex flex-col justify-left w-full">
-                <h1 className="mt-5 ml-5 text-left font-monkeylogo text-monkey-gold font-semibold">search your favorite cocktail</h1>
+                <h1 className="mt-5 ml-5 text-left text-lg font-monkeylogo text-monkey-gold font-semibold">search your favorite cocktail</h1>
                 <div className="max-w-2xl">
-                    <form className="m-5">
+                    <form className="mt-2 mb-5 ml-5 mr-5">
                         <label htmlFor="search"></label>
-                        <SVGSearch css="h-6 w-6 text-monkey-gold" />
-                        <input type="text"
-                            className="w-full"
-                            id="search"
-                            name="search"
-                            value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
-                        />
-
+                        <div className="flex flex-row border-2 border-monkey-gold">
+                            <i className="flex items-center">
+                                <SVGSearch css="ml-1 h-6 w-6 text-monkey-gold" />
+                            </i>
+                            <input type="text"
+                                className="w-full border border-transparent border-opacity-0 focus:ring-transparent focus:border-transparent "
+                                id="search"
+                                name="search"
+                                value={searchTerm}
+                                onChange={(e) => setSearchTerm(e.target.value)}
+                            />
+                        </div>
                     </form>
                 </div>
             </div>
