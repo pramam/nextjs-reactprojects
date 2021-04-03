a36f25ca50895ede769e8efa4b0d9bde82d8247d
pramam <pallavi.ramam@gmail.com>
2021-04-02 21:09:26 -0700

    19: The Monkey Bar: SearchForm, code cleanup

--- components/19-TheMonkeyBar/SearchForm.tsx
+++ components/19-TheMonkeyBar/SearchForm.tsx
@@ -3,7 +3,7 @@ import SVGSearch from '../svgicons/SVGSearch'
 import { useGlobalMonkeyContext } from './Context'
 
 export default function SearchForm() {
-    const { searchTerm, setSearchTerm } = useGlobalMonkeyContext()
+    const { setSearchTerm } = useGlobalMonkeyContext()
     const formInputRef = useRef(null)
 
     const searchCocktail = () => {
@@ -23,7 +23,6 @@ export default function SearchForm() {
     return (
         <section className="mt-10 mx-auto max-w-md md:max-w-2xl shadow-lg flex justify-center">
             <div className="flex flex-col justify-left w-full">
-                {/* <h1 className="mt-5 ml-5 text-left text-lg font-monkeylogo text-monkey-gold font-semibold">search your favorite cocktail</h1> */}
                 <div className="max-w-2xl mt-5 mb-5">
                     <form className="mt-2 mb-5 ml-5 mr-5"
                         onSubmit={handleSubmit}
@@ -40,7 +39,6 @@ export default function SearchForm() {
                                 className="w-full border border-transparent border-opacity-0 focus:ring-transparent focus:border-transparent"
                                 id="search"
                                 name="search"
-                                // value={searchTerm}
                                 ref={formInputRef}
                                 onChange={searchCocktail}
                             />
