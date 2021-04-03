3a5cbabf7a67cc845db54f4de5348d33ead5b673
pramam <pallavi.ramam@gmail.com>
2021-04-03 16:51:26 -0700

    19: The Monkey Bar: Animate Loading with an icon

--- components/19-TheMonkeyBar/Loading.tsx
+++ components/19-TheMonkeyBar/Loading.tsx
@@ -1,5 +1,10 @@
+import SVGRefresh from '../svgicons/SVGRefresh'
+
 export default function Loading() {
     return (
+        <div className="flex flex-row items-center justify-center mx-auto">
+            <SVGRefresh css="mr-2 h-5 w-5 text-monkey-gold animate-spin" />
         <h2 className="text-center mt-5 mb-5 text-lg font-monkeylogo text-monkey-gold font-semibold">Loading...</h2>
+        </div>
     )
 }
