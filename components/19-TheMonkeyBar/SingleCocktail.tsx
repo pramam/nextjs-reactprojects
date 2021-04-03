e5f3fe391edcf56ab5caa4b6aa3590cbdbccf659
pramam <pallavi.ramam@gmail.com>
2021-04-03 15:58:20 -0700

    19: The Monkey Bar: Make the single cocktail page responsive

--- components/19-TheMonkeyBar/SingleCocktail.tsx
+++ components/19-TheMonkeyBar/SingleCocktail.tsx
@@ -90,44 +90,44 @@ export default function SingleCocktail({ id }: IProps) {
                 </a>
             </Link>
             <h1 className="text-center mt-5 mb-5 text-xl font-monkeylogo text-monkey-gold font-semibold">{singleCocktail.name}</h1>
-            <section className="container mx-auto max-w-2xl">
-                <div className="flex flex-row">
+            <section className="container mx-auto sm:max-w-2xl">
+                <div className="flex flex-col mx-5 sm:flex-row lg:mt-10">
                     <img src={singleCocktail.image}
                         alt={singleCocktail.name}
-                        className="w-1/2 rounded-md"
+                        className="w-full sm:w-1/2 rounded-md"
                     />
-                    <div className="flex flex-col justify-between w-1/2">
-                        <div className="ml-5 flex flex-row justify-space-between">
+                    <div className="flex flex-col justify-between w-full sm:w-1/2">
+                        <div className="mt-5 sm:mt-0 ml-5 flex flex-row justify-space-between">
                             <h3 className="flex-grow w-1/2 capitalize text-left text-base font-monkeylogo text-monkey-gold font-semibold">
                                 Name
                             </h3>
                             <p className="w-1/2 text-left">{singleCocktail.name}</p>
                         </div>
-                        <div className="ml-5 flex flex-row">
+                        <div className="mt-2 sm:mt-0 ml-5 flex flex-row">
                             <h3 className="flex-grow w-1/2 capitalize text-left text-base font-monkeylogo text-monkey-gold font-semibold">
                                 Category
                             </h3>
                             <p className="w-1/2 text-left">{singleCocktail.category}</p>
                         </div>
-                        <div className="ml-5 flex flex-row">
+                        <div className="mt-2 sm:mt-0 ml-5 flex flex-row">
                             <h3 className="flex-grow w-1/2 capitalize text-left text-base font-monkeylogo text-monkey-gold font-semibold">
                                 Alcoholic
                             </h3>
                             <p className="w-1/2 text-left">{singleCocktail.alcoholic}</p>
                         </div>
-                        <div className="ml-5 flex flex-row">
+                        <div className="mt-2 sm:mt-0 ml-5 flex flex-row">
                             <h3 className="flex-grow w-1/2 capitalize text-left text-base font-monkeylogo text-monkey-gold font-semibold">
                                 Glass
                             </h3>
                             <p className="w-1/2 text-left">{singleCocktail.glass}</p>
                         </div>
-                        <div className="ml-5 flex flex-row">
+                        <div className="mt-2 sm:mt-0 ml-5 flex flex-row">
                             <h3 className="flex-grow w-1/2 capitalize text-left text-base font-monkeylogo text-monkey-gold font-semibold">
                                 Instructions
                             </h3>
                             <p className="w-1/2 text-left">{singleCocktail.instructions}</p>
                         </div>
-                        <div className="ml-5 flex flex-row">
+                        <div className="mt-2 sm:mt-0 ml-5 flex flex-row">
                             <h3 className="flex-grow w-1/2 capitalize text-left text-base font-monkeylogo text-monkey-gold font-semibold">
                                 Ingredients
                             </h3>
