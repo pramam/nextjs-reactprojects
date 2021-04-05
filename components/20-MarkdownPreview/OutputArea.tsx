ac978bb89b7fc2af6c731d8ee311b44828d7b4bf
pramam <pallavi.ramam@gmail.com>
2021-04-04 21:48:40 -0700

    20: Markdown Preview: Display markdown on left in HTML on right using react-markdown

--- components/20-MarkdownPreview/OutputArea.tsx
+++ components/20-MarkdownPreview/OutputArea.tsx
@@ -1,10 +1,15 @@
+import ReactMarkdown from 'react-markdown'
 import { useGlobalMarkdownContext } from "./Context"
 
 export default function OutputArea() {
     const { markdown } = useGlobalMarkdownContext()
     return (
         <div className="w-full sm:w-1/2 mx-4 sm:mx-5 md:mx-5 lg:mx-10 px-5 pt-5 mt-5 sm:mt-0 min-h-[85vh] lg:min-h-[85vh] bg-opacity-20 sm:bg-opacity-0 bg-white sm:bg-transparent rounded-md shadow-xl">
-            {markdown}
+            <div className="prose">
+                <ReactMarkdown>
+                {markdown}
+                </ReactMarkdown>
+            </div>
         </div>
     )
 }
