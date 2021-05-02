d79e2ec08daed9d70a0892b776b84452b76e2f9a
pramam <pallavi.ramam@gmail.com>
2021-05-01 19:22:20 -0700

    21: Pagination: Buttons, Prev, Next for paginating through the entire data set

--- components/21-Pagination/index.tsx
+++ components/21-Pagination/index.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import useFetch from './useFetch'
 import FollowerCard from './FollowerCard'
+import SVGChevronLeft from '../svgicons/SVGChevronLeft'
+import SVGChevronRight from '../svgicons/SVGChevronRight'
 
 export default function Index() {
     const { loading, data } = useFetch()
@@ -8,13 +10,44 @@ export default function Index() {
     const [pageData, setPageData] = useState([])
 
     useEffect(() => {
+        if (loading) return
         setPageData(data[pageNum])
-    }, [])
+    }, [loading, pageNum])
 
+    const pageClickHandler = (page) => {
+        setPageNum(page)
+    }
 
-    console.log(pageData)
+    const prevPageHandler = () => {
+        setPageNum(oldPage => {
+            let prevPage;
+            if (oldPage === 0)
+                prevPage = data.length - 1
+            else
+                prevPage = oldPage - 1;
+            return prevPage
+        })
+    }
+
+    const nextPageHandler = () => {
+        setPageNum(oldPage => {
+            let nextPage;
+            if (oldPage === data.length - 1)
+                nextPage = 0
+            else
+                nextPage = oldPage + 1
+            return nextPage
+        })
+    }
+    // useEffect(() => {
+    // }, [pageNum])
+
     if (loading)
-        return (<h1>Loading</h1>)
+        return (<h1 className="flex justify-center mx-auto uppercase font-semibold text-xl py-10 text-green-400">Loading...</h1>)
+
+    // console.log(pageData)
+    if (!pageData)
+        return (<h1 className="flex justify-center mx-auto uppercase font-semibold text-xl py-10 text-green-400">Paginating Data...</h1>)
 
     return (
         // <div className="min-h-screen bg-gradient-to-r from-[#cdd1d4]   to-[#cdd1d4] ">
@@ -23,6 +56,7 @@ export default function Index() {
             <h1 className="flex justify-center mx-auto uppercase font-semibold text-xl py-10 text-green-400">Pagination</h1>
             <div className="container mx-auto">
                 {/* Outer  */}
+                {!loading &&
                 <ul className="flex flex-row flex-wrap w-full">
                     {pageData.map((obj) => {
                         return (
@@ -32,6 +66,34 @@ export default function Index() {
 
                     })}
                 </ul>
+                }
+                {loading ? '' :
+                    <div className="flex justify-center flex-row flex-wrap">
+                        <button onClick={prevPageHandler}
+                            className="focus:outline-none border border-transparent">
+                            <SVGChevronLeft css="uppercase text-sm rounded-md h-12 w-12 py-3 px-2 mr-2 bg-green-300 font-semibold text-white" />
+                        </button>
+                        {data.map((obj, index) => {
+                            const base_props = "font-semibold text-white p-3 m-2 focus:outline-none border border-transparent uppercase text-sm rounded-md"
+                            const button_props = {
+                                className: pageNum === index ? `bg-green-500 ${base_props}` : `bg-green-300 ${base_props}`
+                            }
+                            return (
+                                <button
+                                    key={index}
+                                    {...button_props}
+                                    onClick={() => pageClickHandler(index)}
+                                >
+                                    {index + 1}
+                                </button>)
+                        })}
+                        <button onClick={nextPageHandler}
+                            className="focus:outline-none border border-transparent">
+                            <SVGChevronRight css="uppercase text-sm rounded-md h-12 w-12 py-3 px-2 mr-2 bg-green-300 font-semibold text-white" />
+                        </button>
+
+                    </div>
+                }
             </div>
         </div>
     )
--- components/21-Pagination/utils/paginate.ts
+++ components/21-Pagination/utils/paginate.ts
@@ -1,10 +1,10 @@
 export default function paginate(data) {
-    const itemsPerPage = 11
+    const itemsPerPage = 10
     const numPages = Math.ceil(data.length / itemsPerPage)
 
     const newArrayOfArrays = Array.from({ length: numPages }, (_, index) => {
         const start = index * itemsPerPage
-        console.log(start, start + itemsPerPage - 1)
+        // console.log(start, start + itemsPerPage - 1)
 
         return data.slice(start, start + itemsPerPage)
 
