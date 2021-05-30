a8c43fdece322c601e40981efac4e02dbe53909f
pramam <pallavi.ramam@gmail.com>
2021-05-29 19:28:17 -0700

    9: Slider: Added aria-label to L/R arrows for Accessibility

--- components/9-Slider/LeftArrow.tsx
+++ components/9-Slider/LeftArrow.tsx
@@ -4,7 +4,10 @@ export default function LeftArrow() {
     return (
         <div className="rounded-full h-10 w-10 bg-gray-700">
             <div className="px-0.5 py-1">
-                <SVGChevronLeft css="h-8 w-8 text-gray-50" />
+                {/* TODO: i18n */}
+                <span aria-label="Left Arrow">
+                    <SVGChevronLeft aria-hidden="true" css="h-8 w-8 text-gray-50" />
+                </span>
             </div>
         </div>
     )
--- components/9-Slider/RightArrow.tsx
+++ components/9-Slider/RightArrow.tsx
@@ -4,7 +4,10 @@ export default function RightArrow() {
     return (
         <div className="rounded-full h-10 w-10 bg-gray-700">
             <div className="px-0.5 py-1">
-                <SVGChevronRight css="h-8 w-8 text-gray-50" />
+                {/* TODO: i18n */}
+                <span aria-label="Right Arrow">
+                    <SVGChevronRight aria-hidden="true" css="h-8 w-8 text-gray-50" />
+                </span>
             </div>
         </div>
     )
